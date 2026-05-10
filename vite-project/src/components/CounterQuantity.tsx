d08dff4068ff0ee88828c2faba64981e97ee88b9
Igor-off <igor.silva.rafael.701@ufrn.edu.br>
2026-05-10 20:48:08 -0300

    implementação dos botões de quantidade usando a biblioteca lucide-react

---
+++ vite-project/src/components/CounterQuantity.tsx
@@ -0,0 +1,34 @@
+import type React from "react"
+import style from "./CounterQuantity.module.css"
+import { ChevronUp, ChevronDown } from "lucide-react";
+
+type Props = {
+    quantity: number,
+    setQuantity: React.Dispatch<React.SetStateAction<number>>
+}
+
+export default function CounterQuantity({quantity, setQuantity}: Props) {
+    function increase() {
+        setQuantity(quantity + 1);
+    }
+    function decrease() {
+        if(quantity > 1) {
+            setQuantity(quantity - 1)
+        }
+    }
+    return(
+        <div className={style.counterSection}>
+            <span className={style.quantityProduct}>
+                {quantity}
+            </span>
+            <div className={style.quantitiesButton}>
+                <button onClick={increase}>
+                    <ChevronUp size={16}/>
+                </button>
+                <button onClick={decrease}>
+                    <ChevronDown size={16}/>
+                </button>
+            </div>
+        </div>
+    )
+}
